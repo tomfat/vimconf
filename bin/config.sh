9f5824084e5958bb0ee6ecf4c20c7d761e14cb20
azure provisioned user <tomfat@ubuntu.tomfat.d3.internal.cloudapp.net>
2013-11-25 03:17:20 +0000

    add config sh

--- bin/config.sh
+++ bin/config.sh
@@ -3,17 +3,20 @@ cd $(dirname $0)/..
 
 ROOT_DIR=`pwd`
 BIN_DIR=$ROOT_DIR/bin
-VIM_DIR="~/.vim"
-CONF_DIR=$ROOT_DIR/vimfiles
-
-echo $BIN_DIR
-echo $ROOT_DIR
-echo $CONF_DIR
+cd ~
+HOME_DIR=`pwd`
+VIM_DIR=$HOME_DIR/.vim
+VIM_FILES_DIR=$ROOT_DIR/vimfiles
 
 echo "Check folder $VIM_DIR"
 if [ ! -d "$VIM_DIR" ]
 then
 	echo "Create .vim folder..."
-#	mkdir $VIM_DIR
+	mkdir "$VIM_DIR"
 fi
 
+echo "Copy .vimrc"
+cp $ROOT_DIR/_vimrc $HOME_DIR/.vimrc -b
+
+echo "Copy plugin and doc"
+cp $VIM_FILES_DIR/* $VIM_DIR -r

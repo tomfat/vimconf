be16f8de27b0cf6fdc9f7cf3ebcf2ada5eb32fb5
unknown <yuezha@MININT-FODKI14.fareast.corp.microsoft.com>
2014-01-16 12:28:44 +0800

    Switch to using soft symbol link

--- bin/install.bat
+++ bin/install.bat
@@ -9,51 +9,45 @@ REM set VIM_DIR=%CD%\test
 REM in windows vim plugin are placed under vimfiles folder
 set VIM_FILES=%VIM_DIR%\vimfiles
 set AUTOLOAD_DIR=%VIM_FILES%\autoload
-set BUNDLE_DIR=%VIM_FILES%\bundle
+set BUNDLE_DIR=%ROOT_DIR%\bundle
 
 echo "----Create vimfiles directory"
 mkdir %VIM_FILES%
 mkdir %AUTOLOAD_DIR%
 mkdir %BUNDLE_DIR%
 
-echo "----Copy _vimrc file"
-copy "%ROOT_DIR%\_vimrc" "%VIM_DIR%"
-
-REM Install plugins
-REM Switch to vimfiles directory
-pushd %VIM_FILES%
-
 REM pathogen is plugin used to manage other plugin in a neat way
 echo "----Install pathogen"
-git clone git://github.com/tpope/vim-pathogen.git pathogen
-echo "Copy pathogen"
-copy "pathogen\autoload" "%VIM_FILES%\autoload"
+git submodule add git://github.com/tpope/vim-pathogen.git pathogen
 
-REM all other plugin will be installed under bundle folder.
-
-REM Switch to bundle directory
 pushd %BUNDLE_DIR%
-
 echo "----Install delimitmate"
-git clone git://github.com/Raimondi/delimitMate.git delmitmate
+git submodule add git://github.com/Raimondi/delimitMate.git delmitmate
 
 echo "----Install desert color schema"
-git clone git://github.com/vim-scripts/desert.vim.git desert
+git submodule add git://github.com/vim-scripts/desert.vim.git desert
 
 echo "----Install nerdtree"
-git clone git://github.com/scrooloose/nerdtree.git nerdtree
+git submodule add git://github.com/scrooloose/nerdtree.git nerdtree
 
 echo "----Install taglist"
-git clone git://github.com/vim-scripts/taglist.vim.git targlist
+git submodule add git://github.com/vim-scripts/taglist.vim.git targlist
 
 echo "----Install jsbeautify"
-git clone git://github.com/maksimr/vim-jsbeautify.git jsbeautify
-pushd jsbeautify
+git submodule add git://github.com/maksimr/vim-jsbeautify.git jsbeautify
+
+REM return to ROOT_DIR
+popd
 git submodule update --init --recursive
-popd
 
-REM return to previous folder
-popd
-popd
-popd
+echo "----Create link for _vimrc file"
+mklink "%VIM_DIR%\_vimrc" "%ROOT_DIR%\_vimrc" 
 
+echo "----Create link for pathogen"
+mklink "%AUTOLOAD_DIR%\pathogen.vim" "%ROOT_DIR%\pathogen\autoload\pathogen.vim" 
+
+echo "----Create link for other plugins"
+mklink /D "%VIM_FILES%\bundle" "%ROOT_DIR%\bundle" 
+
+REM Return to previous folder
+popd
